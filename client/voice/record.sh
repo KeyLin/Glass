171e0f20b87aada5c27370e2eabe6b528a618f90
root <root@raspberrypi.(none)>
2015-03-26 20:29:09 +0800

    成功录音，并正确返回识别结果

--- client/voice/record.sh
+++ client/voice/record.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-arecord --channels=1 --rate=16000 --duration=3 --format=S16_LE --file-type wav /home/admin/glass/client/voice/cmd.wav
+arecord --channels=1 --rate=16000 --duration=3 --format=S16_LE --file-type wav ./cmd.wav
