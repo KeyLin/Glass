7c30734c2f2d172a699cfc08fe27b3238a6c8e27
killin <linklin@outlook.com>
2015-05-05 22:56:07 +0800

    default

--- client/voice/sendBaidu.py
+++ client/voice/sendBaidu.py
@@ -81,14 +81,9 @@ class sendBaidu(object):
 			text = "".join(result.get('result')).encode('utf-8')
 			#print text
 		else:
-			# err_no = result['err_no']
 			err_msg = "".join(result.get('err_msg')).encode('utf-8')
-			# sn = "".join(result.get('sn')).encode('utf-8')
 			# print "err_no:"+str(err_no)
-			#print "err_msg:"+err_msg
-			# print "sn:"+sn
 			#exit(0)
-		#print str(result)
 
 if __name__ == "__main__":
 	test = sendBaidu(fileFormat = "pcm", audioFile = "data/cmd.pcm")
